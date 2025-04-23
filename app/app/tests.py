274c741ddbc701d38c80b98865f6995911fc9c5a
Adam Pałka <adm.palka@gmail.com>
2025-04-23 11:30:56 +0200

    feat(calc): implement subtraction function and corresponding tests

--- app/app/tests.py
+++ app/app/tests.py
@@ -14,3 +14,9 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting first number from second number."""
+        res = calc.subtract(10, 15)
+
+        self.assertEqual(res, 5)
